45a5f7dd1a116b544cfb67da0db832b4bc4843d8
martucs <martalopezcab@gmail.com>
2025-03-11 18:47:54 +0100

    added delete to main

--- 1/ex00/main.cpp
+++ 1/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: martalop <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 16:18:28 by martalop          #+#    #+#             */
-/*   Updated: 2025/03/11 18:40:17 by martalop         ###   ########.fr       */
+/*   Updated: 2025/03/11 18:47:01 by martalop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,23 @@ int	main(void)
 	std::string	my_name = "Marta";
 	std::string	my_name2 = "lop";
 	// locally creating object	
-	Zombie	my_zombie;
+//	Zombie	my_zombie;
 
-	my_zombie.setName(my_name);
-	my_zombie.announce();
+//	my_zombie.setName(my_name);
+//	my_zombie.announce();
 
 	// malloc'd object
-//	Zombie	*my_zombie;
-//	my_zombie = newZombie(my_name);
-//	my_zombie->announce();
+	Zombie	*my_zombie;
+	my_zombie = newZombie(my_name);
+	my_zombie->announce();
+	delete my_zombie;
+
+	std::cout << std::endl;
 
 	// not malloc'd object
 	randomChump(my_name2);
 
+	std::cout << std::endl;
 	std::cout << "END of program" << std::endl;
 	return (0);
 }
