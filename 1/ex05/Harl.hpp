f2e97b3447c81f0456f3bcf793d8f17209d943bc
martucs <martalopezcab@gmail.com>
2025-03-17 23:22:12 +0100

    ex05 start

---
+++ 1/ex05/Harl.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: martalop <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 23:06:51 by martalop          #+#    #+#             */
+/*   Updated: 2025/03/17 23:17:32 by martalop         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <string>
+
+class Harl
+{
+	private:
+		void	debug( void );
+		void	info( void );
+		void	warning( void );
+		void	error( void );
+
+	public:
+		Harl();
+		~Harl();
+		void	complain( std::string level );
+};
+
+#endif
