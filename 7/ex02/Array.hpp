428c9bfe1e18c7a2b80d8b31decb5d5bfbf0707b
martucs <martalopezcab@gmail.com>
2025-05-10 22:43:41 +0200

    ex02 cpp7 done

--- 7/ex02/Array.hpp
+++ 7/ex02/Array.hpp
@@ -13,7 +13,6 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
-
 #include <iostream>
 
 template <typename T>
@@ -22,21 +21,95 @@ class	Array
 	public:
 		Array();
 		Array(unsigned int n);
-		Array(Array &var);
+		Array(const Array &var);
 		~Array();
 		
 		// member functions
-		int	size(void);
+		unsigned int	size(void) const;
 
 		// operators
-		int&	operator[](int);
-		T&	operator=(const T&);
+		Array&	operator=(const Array<T>&);
+		T&	operator[](int) const;
 	
 	private:
 		// atributes
-		T	*_array;
+		T		*_array;
+		unsigned int	_size;
 };
 
-#include "Array.tpp"
+// INFO: CONSTRUCTORES & DESTRUCTORES
+template <typename T>
+Array<T>::Array()
+{
+	std::cout << "Array constructor called" << std::endl;
+	_array = NULL;
+	_size = 0;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n)
+{
+	std::cout << "Array complete constructor called" << std::endl;
+	if (n > 100000)
+		throw std::overflow_error("Invalid array size");
+	_array = new T[n];
+	_size = n;
+}
+
+template <typename T>
+Array<T>::Array(const Array &var)
+{
+	std::cout << "Array copy constructor called" << std::endl;
+	if (var.size() >= 0)
+	{
+		this->_array = new T[var.size()];
+		*this = var;
+	}
+	else
+	{
+		_array = NULL;
+		_size = 0;
+	}
+}
+
+template <typename T>
+Array<T>::~Array()
+{	
+	std::cout << "Array destructor called" << std::endl;
+	delete[] _array;
+}
+
+// INFO: FUNCIONES MIEMBRO
+template <typename T>
+unsigned int	Array<T>::size(void) const
+{
+	return _size;
+}
+
+// INFO: OPERADORES
+template <typename T>
+Array<T>&	Array<T>::operator=(const Array<T>& var)
+{
+	if (this != &var)
+	{
+		unsigned int i = 0;
+
+		while (i < var.size() && _array != NULL)
+		{
+			_array[i] = var._array[i];
+			i++;
+		}
+		_size = var._size;
+	}
+	return (*this);
+}
+
+template <typename T>
+T&	Array<T>::operator[](int index) const
+{
+	if (index >= static_cast<int>(_size) || index < 0)
+		throw (std::runtime_error("Array index is out of bounds!"));
+	return (_array[index]);
+}
 
 #endif
--- 7/ex02/main.cpp
+++ 7/ex02/main.cpp
@@ -11,11 +11,52 @@
 /* ************************************************************************** */
 
 #include "Array.hpp"
-#include "Array.tpp"
+#include <complex>
 
 int	main()
 {
-	Array	i;
+	try
+	{
+		Array	<int>			int_arr;
+		Array	<std::string>		str_arr(3);
+		Array	<std::complex<double> >	dbl_cmplx_arr(10);
 
+
+		std::cout << std::endl;
+		std::cout << "int array size = " << int_arr.size() << std::endl;
+		//std::cout << int_arr[1] << std::endl;
+		std::cout << "str array size = " << str_arr.size() << std::endl;
+		std::cout << "complex array size = " << dbl_cmplx_arr.size() << std::endl;
+
+		std::cout << std::endl;
+		str_arr[0] = "hola";
+		std::cout << "str[0] = " << str_arr[0] << std::endl;
+		
+		str_arr[1] = "que";
+		std::cout << "str[1] = " << str_arr[1] << std::endl;
+		
+		str_arr[2] = "tal";
+		std::cout << "str[2] = " << str_arr[2] << std::endl;
+		std::cout << std::endl;
+		
+		Array	<std::string>	str_arr_copied(str_arr);
+		
+		std::cout << "str_arr_copied[2] = " << str_arr_copied[2] << std::endl;
+		std::cout << std::endl;
+		
+		dbl_cmplx_arr[0] = 42;
+		std::cout << "complex[0] real = " << dbl_cmplx_arr[0].real() << std::endl;
+		std::cout << "complex[0] imaginary = " << dbl_cmplx_arr[0].imag() << std::endl;
+		std::cout << std::endl;
+		
+	}
+	catch (std::runtime_error &e)
+	{
+		std::cout << "Runtime exception: " << e.what() << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Other exception happened: " << e.what() << std::endl;
+	}
 	return (0);
 }
