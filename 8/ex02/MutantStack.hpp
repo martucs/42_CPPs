d259f37e8d750f486d31a0fe729efbc4d856f011
martucs <martalopezcab@gmail.com>
2025-05-26 21:04:23 +0200

    ex02 cpp8 not yet finished

--- 8/ex02/MutantStack.hpp
+++ 8/ex02/MutantStack.hpp
@@ -1,16 +1,62 @@
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
+#include <iostream>
 #include <stack>
 
-class MutantStack : public std::stack<int>
+template <typename T>
+class MutantStack : public std::stack<T>
 {
 	public:
+		typedef typename std::stack<T>::container_type::iterator	iterator;
 		MutantStack();
 		MutantStack(const MutantStack& );
 		~MutantStack();
-		
+	
+	//	typename std::stack<T>::container_type::iterator	begin();
+	//	typename std::stack<T>::container_type::iterator	end();
+		iterator	begin();
+		iterator	end();
+
 		MutantStack&	operator=(const MutantStack&);
 };
 
+template <typename T>
+MutantStack<T>::MutantStack()
+{
+//	std::cout << "mutantstack constructor called" << std::endl;
+}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack& var)
+{
+	*this = var;
+}
+
+template <typename T>
+MutantStack<T>::~MutantStack()
+{
+}
+
+template <typename T>
+typename std::stack<T>::container_type::iterator	MutantStack<T>::begin(void)
+{
+	return (this->c.begin());
+}
+
+template <typename T>
+typename std::stack<T>::container_type::iterator	MutantStack<T>::end(void)
+{
+	return (this->c.end());
+}
+
+template <typename T>
+MutantStack<T>&		MutantStack<T>::operator=(const MutantStack& var)
+{
+	if (this != &var)
+	{
+		std::stack<T>::operator=(var);
+	}
+	return (*this);
+}
 #endif
--- 8/ex02/main.cpp
+++ 8/ex02/main.cpp
@@ -11,20 +11,95 @@
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
-
 #include <iostream>
+#include <list>
 
-int	main()
+int main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	std::cout  << std::endl;
+	std::cout  << std::endl;
+
+	std::list<int> listt;
+	listt.push_back(5);
+	listt.push_back(17);
+	std::cout << listt.back() << std::endl;
+	listt.pop_front();
+	std::cout << listt.size() << std::endl;
+	listt.push_back(3);
+	listt.push_back(5);
+	listt.push_back(737);
+	//[...]
+	listt.push_back(0);
+	std::list<int>::iterator it = listt.begin();
+	std::list<int>::iterator ite = listt.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> ss(listt);
+	std::cout  << std::endl;
+	return 0;
+}
+
+// INFO:
+// 	MutantStack<int>::iterator it = mstack.begin();
+// esto me da "error: invalid use of non-static data member 'iterator'" porque me faltaba poner typedef en el .hpp
+
+/*int	main()
 {
 	std::stack<int>	realStack;
+	
+	std::deque<int>	Deque;
+	Deque.assign(4, 42);
+
+	std::deque<int>::iterator it = Deque.begin();
+	
+	std::cout << "Deque:" << std::endl;
+	std::cout << *it << std::endl;
+	std::cout << std::endl;
 
 	realStack.push(12);
+	std::cout << "real stack:" << std::endl;
 	std::cout << realStack.top() << std::endl;
 	std::cout << std::endl;
 
-	MutantStack mstack;
+	MutantStack<int> mstack;
 	
 	mstack.push(10);
-	std::cout << mstack.top() << std::endl;
+	mstack.push(20);
+	mstack.push(30);
+	std::cout << "Mutant stack:" << std::endl;
+	std::stack<int>::container_type::iterator myStackItB = mstack.begin();
+	std::stack<int>::container_type::iterator myStackItE = mstack.end();
+
+	while (myStackItB != myStackItE)
+	{
+		std::cout << *myStackItB << std::endl;
+		myStackItB++;
+	}
 	return (0);
-}
+}*/
