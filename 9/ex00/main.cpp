f435e951dd6a64405027d2bf0db1e8bbf54b23e7
martucs <martalopezcab@gmail.com>
2025-05-29 19:50:33 +0200

    ex00 cpp9 start

---
+++ 9/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: martalop <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/29 18:37:27 by martalop          #+#    #+#             */
+/*   Updated: 2025/05/29 18:44:16 by martalop         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	main(int argc, char **argv)
+{
+	// 1. decide how to store database in "./data.csv"
+	// 2. open file 'argv[1]'
+		// if not possible -> "error: could not open file" 
+	// 3. store info
+	// 4. compare each date of file with our database
+		// error if invalid date 
+		// error if negative value
+		// error if num is bigger than int
+		// error if there is no num or date
+	// 5. multiply value * exchange rate 
+	// 6. show result (date -> value = result)
+
+	return (0);
+}
