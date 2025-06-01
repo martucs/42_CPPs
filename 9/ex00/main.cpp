dada3a3b6bda72b5a41d1b30caa9b5c7af068b6b
martucs <martalopezcab@gmail.com>
2025-06-01 20:32:52 +0200

    ex00 cpp9 start storing database

--- 9/ex00/main.cpp
+++ 9/ex00/main.cpp
@@ -10,6 +10,58 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+#include <iostream>
+#include <fstream>
+#include <map>
+
+bool	readFile(char *	filename)
+{
+	std::fstream	inFile;
+
+	inFile.open(filename, std::ios_base::in); 
+	if (inFile.fail())
+	{
+		std::cout << "Error: failed to open file" << std::endl;
+		return (0);
+	}
+	return (1);
+}
+
+std::map<std::string, float>*	storeDataBase(void)
+{
+	std::fstream			dataFile;
+	std::map<std::string, float>	*dataMap;
+	std::string			buffer;
+
+	dataFile.open("./data.csv", std::ios_base::in);
+	
+	dataMap = new std::map<std::string, float>;
+
+	std::getline(dataFile, buffer);
+	buffer.clear();
+	while (true)
+	{
+		std::getline(dataFile, buffer);
+		if (dataFile.eof())
+			break;
+		std::string::size_type	pos;
+		
+		pos = buffer.find(',', 0);
+		dataMap->insert(std::pair<std::string, float>(buffer.substr(0, pos), buffer.substr(pos + 1, buffer.npos)));	
+		buffer.clear();
+	}
+	std::map<std::string, float>::iterator	it;
+	it = dataMap->begin();
+	while (it != dataMap->end())
+	{
+		std::cout << it->first << " -> " << it->second << std::endl;
+		it++;
+	}
+	std::cout << "finished reading file" << std::endl;
+	return (dataMap);
+}
+
 int	main(int argc, char **argv)
 {
 	// 1. decide how to store database in "./data.csv"
@@ -23,6 +75,20 @@ int	main(int argc, char **argv)
 		// error if there is no num or date
 	// 5. multiply value * exchange rate 
 	// 6. show result (date -> value = result)
+	(void)argv;
+	if (argc != 2)
+	{
+		std::cout << "Usage: ./btc infile.txt" << std::endl;
+		return (1);
+	}
+	std::map<std::string, float>	*dataBase;
 
+	dataBase = storeDataBase();
+//	info = storeFileInfo(argv[1]);
+//	if (!info)
+//		return (1);
+
+//	compareData(dataBase);
+	delete dataBase;
 	return (0);
 }
