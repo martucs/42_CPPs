c303fb04ae7407b02cb03bad97d8241fcd6ac4d0
martucs <martalopezcab@gmail.com>
2025-06-03 20:33:27 +0200

    stored database ex00 cpp9

--- 9/ex00/main.cpp
+++ 9/ex00/main.cpp
@@ -14,18 +14,26 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <cstdlib>
 
-bool	readFile(char *	filename)
+bool	readFile(char *	filename, std::fstream& inFile)
 {
-	std::fstream	inFile;
-
 	inFile.open(filename, std::ios_base::in); 
 	if (inFile.fail())
-	{
-		std::cout << "Error: failed to open file" << std::endl;
 		return (0);
-	}
+	inFile.close();
 	return (1);
+}
+
+void	printDataMap(std::map<std::string, float> dataMap)
+{
+	std::map<std::string, float>::iterator	it;
+	it = dataMap.begin();
+	while (it != dataMap.end())
+	{
+		std::cout << it->first << " -> " << it->second << std::endl;
+		it++;
+	}
 }
 
 std::map<std::string, float>*	storeDataBase(void)
@@ -33,11 +41,15 @@ std::map<std::string, float>*	storeDataBase(void)
 	std::fstream			dataFile;
 	std::map<std::string, float>	*dataMap;
 	std::string			buffer;
+	std::string			key;
+	std::string			value;
 
-	dataFile.open("./data.csv", std::ios_base::in);
+	std::cout.setf(std::ios::fixed); 
+	std::cout.precision(2);
 	
 	dataMap = new std::map<std::string, float>;
 
+	dataFile.open("./data.csv", std::ios_base::in);
 	std::getline(dataFile, buffer);
 	buffer.clear();
 	while (true)
@@ -48,47 +60,48 @@ std::map<std::string, float>*	storeDataBase(void)
 		std::string::size_type	pos;
 		
 		pos = buffer.find(',', 0);
-		dataMap->insert(std::pair<std::string, float>(buffer.substr(0, pos), buffer.substr(pos + 1, buffer.npos)));	
+		key = buffer.substr(0, pos);
+		value = buffer.substr(pos + 1, buffer.npos);
+	//	std::cout << "buffer = " << value << std::endl;
+	//	std::cout << "atof = " << std::atof(value.c_str()) << std::endl;
+		dataMap->insert(std::pair<std::string, float>(key, std::atof(value.c_str())));	
 		buffer.clear();
 	}
-	std::map<std::string, float>::iterator	it;
-	it = dataMap->begin();
-	while (it != dataMap->end())
-	{
-		std::cout << it->first << " -> " << it->second << std::endl;
-		it++;
-	}
+//	printDataMap(*dataMap);
 	std::cout << "finished reading file" << std::endl;
 	return (dataMap);
 }
 
 int	main(int argc, char **argv)
 {
-	// 1. decide how to store database in "./data.csv"
+	std::fstream			inFile;
+	std::map<std::string, float>	*dataBase;
+
+	if (argc != 2)
+	{
+		std::cout << "Usage: ./btc infile.txt" << std::endl;
+		return (1);
+	}
+	dataBase = storeDataBase();
+	if (!readFile(argv[1], inFile))	
+	{
+		std::cout << "Error: failed to open file" << std::endl;
+		return (1);
+	}
+//	checkFormat(inFile);
+	delete dataBase;
+	return (0);
+}
+
+// INFO: PROGRAM HAS TO:
+	// 1. store database in "./data.csv" in a std::map
 	// 2. open file 'argv[1]'
 		// if not possible -> "error: could not open file" 
-	// 3. store info
-	// 4. compare each date of file with our database
+	// 3. store info ?
+	// 4. check that file is in correct format
 		// error if invalid date 
 		// error if negative value
 		// error if num is bigger than int
 		// error if there is no num or date
 	// 5. multiply value * exchange rate 
 	// 6. show result (date -> value = result)
-	(void)argv;
-	if (argc != 2)
-	{
-		std::cout << "Usage: ./btc infile.txt" << std::endl;
-		return (1);
-	}
-	std::map<std::string, float>	*dataBase;
-
-	dataBase = storeDataBase();
-//	info = storeFileInfo(argv[1]);
-//	if (!info)
-//		return (1);
-
-//	compareData(dataBase);
-	delete dataBase;
-	return (0);
-}
